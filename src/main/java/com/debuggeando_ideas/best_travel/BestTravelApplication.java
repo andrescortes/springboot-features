748795f68c937df92127af222a05e1997f249454
Miguel Cortes <miguel.cortes@ias.com.co>
2023-06-10 08:38:54 -0500

    deleted spaces

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BestTravelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BestTravelApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BestTravelApplication.class, args);
+    }
 }
