865041e2f1188f2e20c4cca8104e6c31de5004b9
Miguel Cortes <miguel.cortes@ias.com.co>
2023-06-10 08:58:24 -0500

    dependency injection

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -1,12 +1,33 @@
 package com.debuggeando_ideas.best_travel;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class BestTravelApplication {
+public class BestTravelApplication implements CommandLineRunner {
+
+    private final MyService service;
+
+    public BestTravelApplication(MyService service) {
+        this.service = service;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BestTravelApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        service.doSomething();
+    }
+}
+
+@Component
+class MyService {
+
+    public void doSomething() {
+        System.out.println("Hola mundo");
     }
 }
