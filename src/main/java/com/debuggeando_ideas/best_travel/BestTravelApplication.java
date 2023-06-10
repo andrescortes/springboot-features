9296ecbc6e11dbb1aa62c1d001ad13394fcb65fd
Miguel Cortes <miguel.cortes@ias.com.co>
2023-06-10 09:21:43 -0500

    clean up main

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -1,33 +1,12 @@
 package com.debuggeando_ideas.best_travel;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class BestTravelApplication implements CommandLineRunner {
-
-    private final MyService service;
-
-    public BestTravelApplication(MyService service) {
-        this.service = service;
-    }
+public class BestTravelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BestTravelApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        service.doSomething();
-    }
-}
-
-@Component
-class MyService {
-
-    public void doSomething() {
-        System.out.println("Hola mundo");
     }
 }
