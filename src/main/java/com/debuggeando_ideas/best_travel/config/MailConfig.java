9f285f9ddf022a8cd951f827395d75ff2ff94c3e
Miguel Cortes <miguel.cortes@ias.com.co>
2023-07-02 09:39:51 -0500

    inject bean with JavaMailSender

---
+++ src/main/java/com/debuggeando_ideas/best_travel/config/MailConfig.java
@@ -0,0 +1,32 @@
+package com.debuggeando_ideas.best_travel.config;
+
+import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Slf4j
+@Configuration
+public class MailConfig {
+    @Value("${mail.password}")
+    private String mailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("app-travel");
+        mailSender.setPassword(mailPassword);
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+        return mailSender;
+    }
+}
